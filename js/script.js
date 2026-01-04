f4942d62f0f9058b426dc6b9d34419c130ec9588
yasmin-jahan <yasminjahan8603@gmail.com>
2026-01-04 19:37:55 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -121,6 +121,22 @@ $(document).ready(function () {
       },
     });
 
+    // scroll
+ let btn = $('.scroll_button');
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
+
+  btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+  });
+
 });
 
 function updateCartCount() {
